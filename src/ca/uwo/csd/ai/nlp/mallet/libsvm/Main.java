1940d5fc8dd378bf784066a0bebaae9a3bd331bc
Syeed Ibn Faiz <syeedibnfaiz@gmail.com>
2013-10-16 19:12:39 -0700

    Update Main.java

--- src/ca/uwo/csd/ai/nlp/mallet/libsvm/Main.java
+++ src/ca/uwo/csd/ai/nlp/mallet/libsvm/Main.java
@@ -1,6 +1,5 @@
 package ca.uwo.csd.ai.nlp.mallet.libsvm;
 
-
 import cc.mallet.classify.Classifier;
 import cc.mallet.classify.ClassifierTrainer;
 import cc.mallet.pipe.CharSequence2TokenSequence;
@@ -22,26 +21,26 @@ import ca.uwo.csd.ai.nlp.kernel.LinearKernel;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {                
+    public static void main(String[] args) throws IOException, Exception {
         ArrayList<Pipe> pipes = new ArrayList<Pipe>();
         pipes.add(new Target2Label());
         pipes.add(new CharSequence2TokenSequence());
         pipes.add(new TokenSequence2FeatureSequence());
         pipes.add(new FeatureSequence2FeatureVector());
         SerialPipes pipe = new SerialPipes(pipes);
-                
+
         //prepare training instances
-        InstanceList trainingInstanceList = new InstanceList(pipe);        
+        InstanceList trainingInstanceList = new InstanceList(pipe);
         trainingInstanceList.addThruPipe(new CsvIterator(new FileReader("webkb-train-stemmed.txt"),
                 "(.*)\t(.*)", 2, 1, -1));
-        
+
         //prepare test instances
-        InstanceList testingInstanceList = new InstanceList(pipe);        
+        InstanceList testingInstanceList = new InstanceList(pipe);
         testingInstanceList.addThruPipe(new CsvIterator(new FileReader("webkb-test-stemmed.txt"),
                 "(.*)\t(.*)", 2, 1, -1));
-                
+
         ClassifierTrainer trainer = new SVMClassifierTrainer(new LinearKernel());
         Classifier classifier = trainer.train(trainingInstanceList);
-        System.out.println("Accuracy: " + classifier.getAccuracy(testingInstanceList));                
+        System.out.println("Accuracy: " + classifier.getAccuracy(testingInstanceList));
     }
 }
