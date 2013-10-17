b0e1daed57e2ae2dde48393c4198cdf0dcd33739
Syeed Ibn Faiz <syeedibnfaiz@gmail.com>
2013-10-16 19:13:05 -0700

    Update SVMClassifier.java

--- src/ca/uwo/csd/ai/nlp/mallet/libsvm/SVMClassifier.java
+++ src/ca/uwo/csd/ai/nlp/mallet/libsvm/SVMClassifier.java
@@ -1,11 +1,9 @@
 package ca.uwo.csd.ai.nlp.mallet.libsvm;
 
-
 import cc.mallet.classify.Classification;
 import cc.mallet.classify.Classifier;
 import cc.mallet.pipe.Pipe;
 import cc.mallet.types.Instance;
-import cc.mallet.types.Label;
 import cc.mallet.types.LabelAlphabet;
 import cc.mallet.types.LabelVector;
 import ca.uwo.csd.ai.nlp.common.SparseVector;
@@ -14,30 +12,31 @@ import java.util.Map;
 import java.util.Map.Entry;
 import ca.uwo.csd.ai.nlp.libsvm.ex.SVMPredictor;
 import ca.uwo.csd.ai.nlp.libsvm.svm_model;
+import java.io.Serializable;
 
 /**
  * A wrapper for LibSVM classifier.
  * @author Syeed Ibn Faiz
  */
-public class SVMClassifier extends Classifier {
+public class SVMClassifier extends Classifier implements Serializable {
 
     private svm_model model;
-    private Map<Label, Double> mltLabel2svmLabel;       //mapping from Mallet to SVM label
-    private Map<Double, Label> svmLabel2mltLabel;       //mapping from SVM label to Mallet Label
+    private Map<String, Double> mltLabel2svmLabel;       //mapping from Mallet to SVM label
+    private Map<Double, String> svmLabel2mltLabel;       //mapping from SVM label to Mallet Label
     private int[] svmIndex2mltIndex;                    //mapping from SVM Label indices (svm.label) to Mallet Label indices (targetLabelAlphabet)
     private boolean predictProbability;                 //whether probability is predicted by SVM
 
-    public SVMClassifier(svm_model model, Map<Label, Double> mLabel2sLabel, Pipe instancePipe, boolean predictProbability) {
+    public SVMClassifier(svm_model model, Map<String, Double> mLabel2sLabel, Pipe instancePipe, boolean predictProbability) {
         super(instancePipe);
         this.model = model;
         this.mltLabel2svmLabel = mLabel2sLabel;
         this.predictProbability = predictProbability;
-        init();        
+        init();
     }
 
     private void init() {
-        svmLabel2mltLabel = new HashMap<Double, Label>();
-        for (Entry<Label, Double> entry : mltLabel2svmLabel.entrySet()) {
+        svmLabel2mltLabel = new HashMap<Double, String>();
+        for (Entry<String, Double> entry : mltLabel2svmLabel.entrySet()) {
             svmLabel2mltLabel.put(entry.getValue(), entry.getKey());
         }
 
@@ -46,7 +45,7 @@ public class SVMClassifier extends Classifier {
         LabelAlphabet labelAlphabet = getLabelAlphabet();
         for (int sIndex = 0; sIndex < sLabels.length; sIndex++) {
             double sLabel = sLabels[sIndex];
-            Label mLabel = svmLabel2mltLabel.get(sLabel * 1.0);
+            String mLabel = svmLabel2mltLabel.get(sLabel * 1.0);
             int mIndex = labelAlphabet.lookupIndex(mLabel.toString(), false);
             svmIndex2mltIndex[sIndex] = mIndex;
         }
@@ -56,18 +55,19 @@ public class SVMClassifier extends Classifier {
     public Classification classify(Instance instance) {
         SparseVector vector = SVMClassifierTrainer.getVector(instance);
         double[] scores = new double[model.nr_class];
-        double sLabel = mltLabel2svmLabel.get(getLabelAlphabet().lookupLabel(instance.getTarget().toString()));
+
+        double sLabel = mltLabel2svmLabel.get(getLabelAlphabet().lookupLabel(instance.getTarget().toString()).toString());
         double p = SVMPredictor.predictProbability(new ca.uwo.csd.ai.nlp.libsvm.ex.Instance(sLabel, vector), model, scores);
-        
+
         //if SVM is not predicting probability then assign a score of 1.0 to the best class(p)
         //and 0.0 to the other classes
         if (!predictProbability) {
-            Label label = svmLabel2mltLabel.get(p);
-            int index = getLabelAlphabet().lookupIndex(label.toString(), false);
+            String label = svmLabel2mltLabel.get(p);
+            int index = getLabelAlphabet().lookupIndex(label, false);
             scores[index] = 1.0;
         } else {
             rearrangeScores(scores);
-        }        
+        }
         Classification classification = new Classification(instance, this,
                 new LabelVector(getLabelAlphabet(), scores));
 
